56bb16ab6ccaf73ec58b5aed4c6cbaf2a8dc6d49
Nicole Stone <87792051+Nicole399@users.noreply.github.com>
2021-08-14 13:26:17 +1200

    Update TestCalc.java

--- src/test/java/Tut4pkg/TestCalc.java
+++ src/test/java/Tut4pkg/TestCalc.java
@@ -2,7 +2,7 @@ package Tut4pkg;
 
 
 //import static org.junit.Assert.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
